1a83cf8c7a9aaf53288dcfca7da6f29e8b06815d
grdeveloper <you@example.com>
2018-10-14 20:08:59 +0400

    Final commit

---
+++ public/js/main.js
@@ -0,0 +1,35 @@
+const numberInput = document.getElementById('number');
+const textInput = document.getElementById('msg');
+const button = document.getElementById('button');
+const response = document.querySelector('.response');
+
+button.addEventListener('click', send, false);
+
+const socket = io();
+socket.on('smsStatus', (data) => {
+    response.innerHTML = `<h5>
+                            Text message sent to ${data.number}
+                          </h5>`;
+});
+
+function send() {
+    const number = numberInput.value.replace(/\D/g, '');
+    const text = textInput.value;
+
+    fetch('/', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            number: number,
+            text: text
+        })
+    })
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
